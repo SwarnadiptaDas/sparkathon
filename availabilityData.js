39ebdbced74691fe3293f5f9e5149636569e27ac
SwarnadiptaDas <157202616+SwarnadiptaDas@users.noreply.github.com>
2025-07-03 12:15:14 +0530

    Add files via upload

---
+++ availabilityData.js
@@ -0,0 +1,11 @@
+  
+
+const availabilityData = {
+  "Mumbai": [1, 3, 4, 7, 8],
+  "Bangalore": [2, 3, 5, 6, 9],
+  "Delhi": [1, 2, 4, 6, 7],
+  "Hyderabad": [3, 5, 6, 8, 9],
+  "Chennai": [1, 4, 5, 7, 9],
+};
+
+export default availabilityData;
